8d0c7aef8601154b7c9c12db8fdfcf6ed1275aba
lonord <lonord@qq.com>
2017-10-08 19:26:40 +0800

    Pass `onOverrideContent` prop to trigger item

--- src/index.tsx
+++ src/index.tsx
@@ -62,7 +62,7 @@ export default function createPicker(options: PickOptions) {
 			<Trigger onClick={() => {
 				onOverrideContent(Picker)
 				d('Show sub menu')
-			}} theme={theme} />
+			}} theme={theme} onOverrideContent={onOverrideContent} />
 		)
 	}
 }
